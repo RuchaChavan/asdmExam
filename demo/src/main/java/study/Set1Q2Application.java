b7e26a39d29b24b080fa07443b0bc2c88e304d3e
rucha chavan <ruchachavan19@gmail.com>
2022-03-12 15:14:30 +0530

    add new demo java pro

---
+++ demo/src/main/java/study/Set1Q2Application.java
@@ -0,0 +1,13 @@
+package study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Set1Q2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Set1Q2Application.class, args);
+	}
+
+}
---
+++ demo/src/test/java/study/Set1Q2ApplicationTests.java
@@ -0,0 +1,13 @@
+package study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Set1Q2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
